f19a428cbb7589156935c8ecf29c5366fe6ae722
chris peele <chris.peele79@gmail.com>
2025-05-17 15:09:25 -0500

    feat: add dark mode support and toggle

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,4 +1,5 @@
 export { TitleCard } from './TitleCard/TitleCard';
 export { Skeleton } from './Skeleton/Skeleton';
 export { Avatar, AvatarGroup } from './Avatar';
+export { DarkModeToggle } from './DarkModeToggle/DarkModeToggle';
 export * from './ShadCnBlocks';
